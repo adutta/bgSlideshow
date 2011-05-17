00d696f9286a818a992a70b44a130e4a7d1de8e3
Arun Dutta <adutta13@gmail.com>
2011-05-17 16:00:25 -0400

    Code will just add all pictures in the folder to the xml file.

--- delbg.py
+++ delbg.py
@@ -1,47 +1,40 @@
+# Will create an xml file for a whole folder of pictures (.jpg, .png, and .bmp)
+
 import os
-
-
 
 stop = 0
 while(!stop):
-    cmd = str(raw_input('[C]REATE, [A]DD, or [S]TOP?'))
-
     if cmd == 'C' or cmd == 'c':
         #WHAT IF FILE ALREADY EXISTS?????
+        
         loc = str(raw_input('Please enter the name & location of the file.'))
         if loc[-4:] != '.xml':
             loc += '.xml'
 
-        bgfile = open(loc, 'w')
+        #Ask for folder location
+        picDir = str(raw_input('Please enter the picture directory.'))
+        
+        dur = str(raw_input('How many miliseconds shall each picture last?'))
+
+        randy = str(raw_input('Randomize? (y/n)'))
+
+        bgfile = open(loc, 'w')        
 
         #Start the file contents
         initXML(bgfile, loc)
 
-        #Ask for first file
-        gimmeFile = 'Please enter the name & location of the first picture.'
-        pic1 = str(raw_input(gimmeFile))
-        dur = str(raw_input('How many miliseconds shall it last?'))
+        #Get pics into a list
+        
+        
 
         #Add first file
-        addPic(bgfile, pic1, dur)
+        for x in picL:
+            addPic(bgfile, x, dur)
 
-        #End file to prevent stupid
+        #End file
         addEnd(bgfile, pic1, pic1, dur)
 
-        print 'Your file has been created!  Now add more pictures!!!!!!!!!'
-
         bgfile.close()
-        
-        
-    elif cmd == 'A' or cmd == 'a':
-        bgfile = open(loc, 'a+')
-
-    elif cmd == 'S' or cmd == 's':
-        stop = 1
-        print 'Goodbye!'
-
-    else
-        print 'Invalid command.'
 
 #===========================<Function library>==================================
 def initXML(ofile, loc):
@@ -68,9 +61,3 @@ def addEnd(ofile, loc1, loc2, dur)
     ofile.write('<transition>\n\t<duration>' + dur + '</duration>')
     ofile.write('\n\t<from>' + loc + '</from>')
     ofile.write('\n\t<to>' + loc2 + '</to>\n</transition>')
-
-# Remove the ending for more pics
-def remEnd(ofile)
-    
-    os.remove(temp)
-    
